943a32051c6672b60ad0b599d8d0f8f339809668
ErikTitan <102426162+ErikTitan@users.noreply.github.com>
2025-03-12 20:38:38 +0100

    Initial project setup with dependencies

---
+++ resources/js/pages/Home.vue
@@ -0,0 +1,18 @@
+<script lang="ts">
+import Button from "primevue/button";
+export default {
+  components: {
+    Button
+  }
+}
+</script>
+
+<template>
+    <div class="card flex justify-center align-center pt-4">
+      <Button label="Click" />
+    </div>
+  </template>
+
+<style scoped>
+
+</style>
