944fedcb692a6f50e27ea888d806aca3d93eeee4
Rikito001 <simi.kovacik@gmail.com>
2025-04-07 12:46:53 +0200

    Added simple login and register API

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
 // Public route - does not require authentication
 Route::get('/test', function (Request $request) {
@@ -11,3 +12,7 @@ Route::get('/test', function (Request $request) {
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('/register', [AuthController::class, 'register']);
+
+Route::post('/login', [AuthController::class, 'login']);
