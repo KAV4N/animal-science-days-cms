075fae044361aaab087227e000819c06fb8f2d40
ErikTitan <102426162+ErikTitan@users.noreply.github.com>
2025-04-14 17:39:32 +0200

    Integrate PrimeVue and Tailwind CSS for enhanced UI

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -2,15 +2,18 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import PrimeVue from 'primevue/config';
+import { primevueConfig } from './plugins/primevue-config';
 
 import App from './App.vue'
 import router from './router'
 
-import './plugins/axios'; 
+import './plugins/axios';
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(PrimeVue, primevueConfig);
 
 app.mount('#app')
